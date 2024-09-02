682238a19bd4bc32dfece7c144c9b2f5545461ac
nikol2906333 <nikol2906333@gmail.com>
2024-09-02 12:50:21 +0000

    Remove CORS

--- src/main.py
+++ src/main.py
@@ -28,10 +28,6 @@ def proxy():
         # Прокси запрос на целевой URL
         response = requests.get(target_url)
 
-        # Убираем заголовок X-Frame-Options из заголовков ответа
-        headers = dict(response.headers)
-        headers.pop('X-Frame-Options', None)
-
         # Переписывание путей к статическим ресурсам
         content = response.text
         base_url = target_url.rsplit('/', 1)[0] + '/'
@@ -42,7 +38,7 @@ def proxy():
         app.logger.info(f"Successfully proxied request to: {target_url}")
         
         # Возвращение измененного контента обратно клиенту
-        return Response(content, status=response.status_code, headers=headers)
+        return Response(content, status=response.status_code, content_type=response.headers.get('Content-Type'))
     except requests.exceptions.RequestException as e:
         app.logger.error(f"Error during proxying: {e}")
         return f"Error: {str(e)}", 500
