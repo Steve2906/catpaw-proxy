7c738df55d31f6a7edd2af35f473e0eb9dd71d79
nikol2906333 <nikol2906333@gmail.com>
2024-09-02 10:22:08 +0000

    Update proxy, logs

--- src/main.py
+++ src/main.py
@@ -1,25 +1,40 @@
-import os
 from flask import Flask, request, Response
 import requests
+import logging
 
 app = Flask(__name__)
 
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
-def proxy(path):
-    """Проксирование запросов."""
-    target_url = request.url.replace(request.host_url, "")
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+
+@app.route('/proxy')
+def proxy():
+    target_url = request.args.get('url')
+    
     if not target_url:
+        app.logger.warning("Запрос без URL")
         return "URL is required", 400
 
+    # Логирование поступившего URL
+    app.logger.info(f"Received URL to proxy: {target_url}")
+
+    # Убедитесь, что URL начинается с 'http://' или 'https://'
+    if not target_url.startswith(('http://', 'https://')):
+        app.logger.warning(f"Некорректный URL: {target_url}")
+        return "Invalid URL: URL must start with 'http://' or 'https://'", 400
+
     try:
-        # Выполнение проксируемого запроса к целевому URL
+        # Прокси запрос на целевой URL
         response = requests.get(target_url)
 
-        # Возвращение контента ответа с сохранением статуса и типа содержимого
+        # Логирование успешного запроса
+        app.logger.info(f"Successfully proxied request to: {target_url}")
+        
+        # Возвращение оригинального контента обратно клиенту
         return Response(response.content, status=response.status_code, content_type=response.headers.get('Content-Type'))
     except requests.exceptions.RequestException as e:
+        app.logger.error(f"Error during proxying: {e}")
         return f"Error: {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8083)
+    app.run(host="0.0.0.0", port=8083)
